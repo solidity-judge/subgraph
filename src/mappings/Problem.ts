9e4eed7362af7d0bdb601c7dd83b79a462c1a164
Le Duy Thuc <leduythuc.cs@gmail.com>
2023-05-21 13:33:46 +0700

    Subgraph for v2

--- src/mappings/Problem.ts
+++ src/mappings/Problem.ts
@@ -1,9 +1,6 @@
-import { log } from "@graphprotocol/graph-ts";
+import { BigInt, log } from "@graphprotocol/graph-ts";
 import { Problem, Submission, TestVersion } from "../../generated/schema";
-import {
-  NewTestVersion,
-  RunSolution,
-} from "../../generated/templates/Problem/Problem";
+import { NewTestVersion, RunSolution } from "../../generated/templates/Problem/Problem";
 import { Problem as ProblemContract } from "../../generated/templates/Problem/Problem";
 import { generateTransactionId, setMetaDataFields, setSyncingIndex } from "../helper";
 
@@ -17,11 +14,22 @@ export function handleRunSolution(event: RunSolution): void {
   let submission = new Submission(generateTransactionId(event));
   submission.problem = problem.id;
   submission.contestant = event.params.contestant.toHexString();
-  submission.version = event.params.version;
   submission.point = event.params.point;
+  submission.isPreDeadlineSolution = event.params.isPreDeadlineSolution;
+
   // read solution from the contract
   let contract = ProblemContract.bind(event.address);
-  submission.solution = contract.getContestantInfo(event.params.contestant).getSolution().toHexString();
+  if (event.params.isPreDeadlineSolution) {
+    submission.solution = contract
+      .getContestantInfo(event.params.contestant)
+      .getSolutionPreDeadline()
+      .toHexString();
+  } else {
+    submission.solution = contract
+      .getContestantInfo(event.params.contestant)
+      .getSolutionPosDeadline()
+      .toHexString();
+  }
 
   submission.verdicts = event.params.verdicts;
   setMetaDataFields(submission, event);
@@ -33,6 +41,15 @@ export function handleNewTestVersion(event: NewTestVersion): void {
   let testVersion = new TestVersion(generateTransactionId(event));
   testVersion.problem = event.address.toHexString();
   testVersion.version = event.params.version;
+
+  let maxGasLimit = new BigInt(0);
+  for (let i = 0; i < event.params.tests.length; i++) {
+    if (event.params.tests[i].gasLimit > maxGasLimit) {
+      maxGasLimit = event.params.tests[i].gasLimit;
+    }
+  }
+  testVersion.gasLimit = maxGasLimit;
+
   setMetaDataFields(testVersion, event);
   setSyncingIndex("testVersions", testVersion);
   testVersion.save();
